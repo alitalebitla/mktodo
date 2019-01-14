cabebb8523fe4f92620d6f4d54411ab8afb82ccf
aliTalebi <talebi.ali.prv@gmail.com>
2019-01-15 03:19:06 +0330

    Updates

---
+++ precache-manifest.498e46a6e9cd3fcb78b2aece5fbbe37e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aa2970afdc7cea41a6fb",
+    "url": "/mktodo/static/js/runtime~main.aa2970af.js"
+  },
+  {
+    "revision": "c9f4687db251f0694cc5",
+    "url": "/mktodo/static/js/main.c9f4687d.chunk.js"
+  },
+  {
+    "revision": "98a5d3fb1698179c1e5e",
+    "url": "/mktodo/static/js/1.98a5d3fb.chunk.js"
+  },
+  {
+    "revision": "c9f4687db251f0694cc5",
+    "url": "/mktodo/static/css/main.38d44643.chunk.css"
+  },
+  {
+    "revision": "433bdde41aac6ca58d0f1369262fa6a8",
+    "url": "/mktodo/index.html"
+  }
+];

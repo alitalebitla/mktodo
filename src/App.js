82731c03333336985f4480f54e4d9a3a4e459b06
ali talebi <talebi.ali.prv@gmail.com>
2020-06-28 22:01:41 +0430

    make template

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import {observer} from 'mobx-react'
+import './App.css';
+import todoStore from "./stores/TodoStore";
+
+@observer
+class App extends Component {
+    render() {
+        return [
+            <div id="todoapp" className="todoapp">
+                {/* your code */}
+            </div>,
+            <footer className="info">
+                <p>Written by <a href="#">Your Name</a></p>
+                <p>Project of <a href="https://maktabkhooneh.org">Maktabkhooneh</a></p>
+            </footer>
+        ]
+    }
+}
+
+
+export default App;
---
+++ src/stores/TodoStore.js
@@ -0,0 +1,9 @@
+import {observable, action, computed} from 'mobx'
+import TodoModel from "./TodoModel";
+
+class TodoStore {
+    // Your code here
+}
+
+const todoStore = new TodoStore()
+export default todoStore

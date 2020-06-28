4003af8249808f5285743ae9936b98be3013e3d3
ali talebi <talebi.ali.prv@gmail.com>
2020-06-28 22:41:08 +0430

    add readme

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
     render() {
         return [
             <div id="todoapp" className="todoapp">
-                {/* your code */}
+                {/* your code here */}
+                <h1>Worked :)</h1>
             </div>,
             <footer className="info">
                 <p>Written by <a href="#">Your Name</a></p>
